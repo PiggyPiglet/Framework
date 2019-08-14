de55871d5b1e1b2eaadd21ba44c29cff5cd16ae5
PiggyPiglet <noreply@piggypiglet.me>
2019-08-14 20:34:42 +0800

    convert test to plugin

--- test/src/main/java/me/piggypiglet/test/Test.java
+++ test/src/main/java/me/piggypiglet/test/Test.java
@@ -1,13 +1,15 @@
 package me.piggypiglet.test;
 
 import me.piggypiglet.framework.Framework;
+import org.bukkit.plugin.java.JavaPlugin;
 
 // ------------------------------
 // Copyright (c) PiggyPiglet 2019
 // https://www.piggypiglet.me
 // ------------------------------
-public final class Test {
-    public static void main(String[] args) {
+public final class Test extends JavaPlugin {
+    @Override
+    public void onEnable() {
         Framework.builder()
                 .main(Test.class)
                 .pckg("me.piggypiglet.test")
