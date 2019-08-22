86200328b1ad2d268453b24a1847f1cae3b280d4
PiggyPiglet <noreply@piggypiglet.me>
2019-08-22 16:47:52 +0800

    Fixed sponge commands

--- sponge/src/main/java/me/piggypiglet/framework/sponge/commands/SpongeCommandExecutor.java
+++ sponge/src/main/java/me/piggypiglet/framework/sponge/commands/SpongeCommandExecutor.java
@@ -1,26 +1,58 @@
 package me.piggypiglet.framework.sponge.commands;
 
 import com.google.inject.Inject;
+import me.piggypiglet.framework.Framework;
 import me.piggypiglet.framework.access.AccessManager;
 import me.piggypiglet.framework.sponge.user.SpongeUser;
+import org.spongepowered.api.command.CommandCallable;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
-import org.spongepowered.api.command.args.CommandContext;
-import org.spongepowered.api.command.args.GenericArguments;
-import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 // ------------------------------
 // Copyright (c) PiggyPiglet 2019
 // https://www.piggypiglet.me
 // ------------------------------
-public final class SpongeCommandExecutor implements CommandExecutor {
+public final class SpongeCommandExecutor implements CommandCallable {
     @Inject private AccessManager accessManager;
+    @Inject private Framework framework;
 
     @Override
-    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-        String message = (String) args.getOne("args").orElse("");
-        accessManager.processCommand("sponge", new SpongeUser(src), message);
+    public CommandResult process(CommandSource source, String arguments) throws CommandException {
+        accessManager.processCommand("sponge", new SpongeUser(source), framework.getCommandPrefix() + " " + arguments);
         return CommandResult.success();
+    }
+
+    @Override
+    public List<String> getSuggestions(CommandSource source, String arguments, @Nullable Location<World> targetPosition) throws CommandException {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean testPermission(CommandSource source) {
+        return true;
+    }
+
+    @Override
+    public Optional<Text> getShortDescription(CommandSource source) {
+        return Optional.of(Text.of("Main command."));
+    }
+
+    @Override
+    public Optional<Text> getHelp(CommandSource source) {
+        return Optional.of(Text.of("Main command."));
+    }
+
+    @Override
+    public Text getUsage(CommandSource source) {
+        return Text.of("<required args> [optional args]");
     }
 }
--- sponge/src/main/java/me/piggypiglet/framework/sponge/registerables/CommandExecutorRegisterable.java
+++ sponge/src/main/java/me/piggypiglet/framework/sponge/registerables/CommandExecutorRegisterable.java
@@ -7,30 +7,21 @@ import me.piggypiglet.framework.registerables.StartupRegisterable;
 import me.piggypiglet.framework.sponge.commands.SpongeCommandExecutor;
 import me.piggypiglet.framework.utils.annotations.Main;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.command.args.GenericArguments;
-import org.spongepowered.api.command.spec.CommandSpec;
-import org.spongepowered.api.text.Text;
-
-import java.util.Arrays;
 
 // ------------------------------
 // Copyright (c) PiggyPiglet 2019
 // https://www.piggypiglet.me
 // ------------------------------
 public final class CommandExecutorRegisterable extends StartupRegisterable {
-    @Inject private SpongeCommandExecutor spongeCommandExecutor;
-    @Inject @Main private Object main;
-    @Inject private Framework framework;
     @Inject private AccessManager accessManager;
+    @Inject @Main private Object main;
+    @Inject private SpongeCommandExecutor spongeCommandExecutor;
+    @Inject private Framework framework;
 
     @Override
     protected void execute() {
-        CommandSpec.Builder command = CommandSpec.builder()
-                .arguments(GenericArguments.string(Text.of("args")))
-                .executor(spongeCommandExecutor);
-
         accessManager.newHandler("sponge", injector);
 
-        Sponge.getCommandManager().register(main, command.build(), framework.getCommandPrefix());
+        Sponge.getCommandManager().register(main, spongeCommandExecutor, framework.getCommandPrefix());
     }
 }
