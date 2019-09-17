0a1607fa2cb04e8f3275d77980a45ab75112ee90
PiggyPiglet <noreply@piggypiglet.me>
2019-09-17 18:02:49 +0800

    Made version based on root build.gradle, to prevent outdated poms

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ $# -ne 3 ]; then
-  echo "name, version, and core version not supplied"
+if [ $# -ne 2 ]; then
+  echo "name and version not supplied"
   exit 1
 fi
 
@@ -18,7 +18,7 @@ do
   IFS="/" read -ra cutName <<< "$i"
   cp maven/${cutName[-1]}/pom.xml maven/tmp/pom.xml
   sed -i "s/@VERSION@/$2/g" maven/tmp/pom.xml
-  sed -i "s/@CORE@/$3/g" maven/tmp/pom.xml
+  sed -i "s/@CORE@/$2/g" maven/tmp/pom.xml
   mvn deploy:deploy-file -DgroupId=me.piggypiglet -DartifactId=framework-${cutName[-1]} -Dversion=$2 -Dpackaging=jar -Dfile=$i/build/libs/${cutName[-1]}-$2.jar -DpomFile=maven/tmp/pom.xml -DrepositoryId=piggypiglet -Durl=https://repo.piggypiglet.me/repository/maven-releases
   rm maven/tmp/pom.xml
 done
