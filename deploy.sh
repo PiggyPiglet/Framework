e302347496add6689ad5b974ccc74904662b4f63
PiggyPiglet <noreply@piggypiglet.me>
2019-10-25 16:36:14 +0800

    bump version

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,16 @@ IFS="," read -ra my_array <<< "$names"
 for i in "${my_array[@]}"
 do
   IFS="/" read -ra cutName <<< "$i"
-  cp maven/${cutName[-1]}/pom.xml maven/tmp/pom.xml
+
+  name=${cutName[-1]}
+
+  if [[ $i == *"-common"* ]]; then
+    name=${cutName[-2]}+"-common"
+  fi
+
+  cp maven/${name}/pom.xml maven/tmp/pom.xml
   sed -i "s/@VERSION@/$2/g" maven/tmp/pom.xml
   sed -i "s/@CORE@/$2/g" maven/tmp/pom.xml
-  mvn deploy:deploy-file -DgroupId=me.piggypiglet -DartifactId=framework-${cutName[-1]} -Dversion=$2 -Dpackaging=jar -Dfile=$i/build/libs/${cutName[-1]}-$2.jar -DpomFile=maven/tmp/pom.xml -DrepositoryId=piggypiglet -Durl=https://repo.piggypiglet.me/repository/maven-releases
+  mvn deploy:deploy-file -DgroupId=me.piggypiglet -DartifactId=framework-$name -Dversion=$2 -Dpackaging=jar -Dfile=$i/build/libs/$name-$2.jar -DpomFile=maven/tmp/pom.xml -DrepositoryId=piggypiglet -Durl=https://repo.piggypiglet.me/repository/maven-releases
   rm maven/tmp/pom.xml
 done
