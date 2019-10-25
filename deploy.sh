28a014aae54d57bf15b199884b3d80661df2d814
PiggyPiglet <noreply@piggypiglet.me>
2019-10-25 16:39:39 +0800

    added support for more nested modules in deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,13 +20,13 @@ do
 
   name=${cutName[-1]}
 
-  if [[ $i == *"-common"* ]]; then
-    name=${cutName[-2]}+"-common"
+  if [[ $name == "common" ]]; then
+    name="${cutName[-2]}-common"
   fi
 
   cp maven/${name}/pom.xml maven/tmp/pom.xml
   sed -i "s/@VERSION@/$2/g" maven/tmp/pom.xml
   sed -i "s/@CORE@/$2/g" maven/tmp/pom.xml
-  mvn deploy:deploy-file -DgroupId=me.piggypiglet -DartifactId=framework-$name -Dversion=$2 -Dpackaging=jar -Dfile=$i/build/libs/$name-$2.jar -DpomFile=maven/tmp/pom.xml -DrepositoryId=piggypiglet -Durl=https://repo.piggypiglet.me/repository/maven-releases
+  mvn deploy:deploy-file -DgroupId=me.piggypiglet -DartifactId=framework-$name -Dversion=$2 -Dpackaging=jar -Dfile=$i/build/libs/${cutName[-1]}-$2.jar -DpomFile=maven/tmp/pom.xml -DrepositoryId=piggypiglet -Durl=https://repo.piggypiglet.me/repository/maven-releases
   rm maven/tmp/pom.xml
 done
