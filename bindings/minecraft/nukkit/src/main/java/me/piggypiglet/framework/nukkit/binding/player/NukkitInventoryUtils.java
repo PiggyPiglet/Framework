ff85c32cfff08ed64bf48af08ed3b37bba52c172
PiggyPiglet <noreply@piggypiglet.me>
2019-11-30 23:16:17 +0800

    Added missing license headers

--- bindings/minecraft/nukkit/src/main/java/me/piggypiglet/framework/nukkit/binding/player/NukkitInventoryUtils.java
+++ bindings/minecraft/nukkit/src/main/java/me/piggypiglet/framework/nukkit/binding/player/NukkitInventoryUtils.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 PiggyPiglet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package me.piggypiglet.framework.nukkit.binding.player;
 
 import cn.nukkit.item.enchantment.Enchantment;
@@ -5,9 +29,9 @@ import cn.nukkit.nbt.tag.Tag;
 import me.piggypiglet.framework.minecraft.player.inventory.objects.Item;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.HashSet;
 
 public final class NukkitInventoryUtils {
     static void transferMap(Map<Integer, Item> og, Map<Integer, Item> inven, int initial, int end) {
